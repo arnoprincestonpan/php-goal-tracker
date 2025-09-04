29c739609b53908dba434415d5c47ead3b66b2b9
Arno Pan <apan11@my.bcit.ca>
2025-09-04 16:06:37 -0700

    Added logic for  creation if doesn't exist. Instead of depending on manually coding on mySQL (MariaDB). Changed logic for  connection check not to include database. Current Issue: Retrieving  in index.php crashing because  is empty or could be empty

--- connect.php
+++ connect.php
@@ -1,21 +1,34 @@
 <?php
 $user = "root";
 $password = "root";
-$db = "goals";
 $host = "localhost";
 $port = 8080;
 
 $link = mysqli_init();
+if(!$link){
+    die("mysqli_init failed; Check PHP Environment for mysqli module.");
+}
+
+// Check if we can connect to MySQL (MariaDB), don't put $db just yet
 $success = mysqli_real_connect(
     $link,
     $host,
     $user,
     $password,
-    $db,
+    null,
     $port
 );
 
 if(!$success){
     die("Connection Failed: " . mysqli_connect_error());
+}
+
+$db = "goals";
+$sql_create_db = "CREATE DATABSE IF NOT EXISTS " . $db;
+
+if($link->query($sql_create_db) === TRUE){
+    "<script>$db created or already exists. Proceeding...</script>";
+} else {
+    die("Error creating table: " . $link->error);
 }
 ?>
