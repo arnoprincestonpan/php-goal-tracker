8e894d09ae85457c799b6d5daffe22bc5f2d97d3
pan.arno@gmail.com <pan.arno@gmail.com>
2025-09-05 00:24:56 -0700

    Prevent Sequel Injection. Prepare statement.

--- insert_goal.php
+++ insert_goal.php
@@ -10,13 +10,25 @@ if(empty($date)){
 }
 $complete = $_POST['complete'] ?? 0;
 
-$sql = "INSERT INTO goals(goal_category, goal_text, goal_date, goal_complete) VALUES ('$category', '$text', '$date', '$complete')";
+// add placeholders to prevent sequel injection
+$sql = "INSERT INTO goals(goal_category, goal_text, goal_date, goal_complete) VALUES (?, ?, ?, ?)";
 
-// Execute the Query (inserting) and see if it suceeded or failed
-if(mysqli_query($link, $sql)){
-    print("Stored");
+// initialize statement and prepare for execution 
+if($stmt = mysqli_prepare($link, $sql)){
+    // prepare statement, put types, string string string integer
+    mysqli_stmt_bind_param($stmt, "sssi", $category, $text, $date, $complete);
+
+    // Execute the Query (inserting) and see if it suceeded or failed
+    if(mysqli_stmt_execute($stmt)){
+        print("Stored");
+    } else {
+        print("Failed");
+    }
+
+    // Close statement
+    mysqli_stmt_close($stmt);
 } else {
-    print("Failed");
+    print("Error preparing statement: " . mysqli_error($link));
 }
 
 // Take us back to the main page index.php
